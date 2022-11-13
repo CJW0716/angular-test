5e0633c6656f3ef78a923e04d57c65f6997d8b14
CJW0716 <49069371+CJW0716@users.noreply.github.com>
2022-11-13 17:00:39 +0800

    【Message】: 检索运费价格；创建配送组件

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 import { Product } from './products';
 @Injectable({
   providedIn: 'root'
@@ -6,7 +7,10 @@ import { Product } from './products';
 export class CartService {
 
   items: Product[]=[];
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
   // 添加到购物车
   addToCart(product:Product){
     this.items.push(product);
@@ -19,6 +23,11 @@ export class CartService {
   clearCart(){
     this.items = [];
     return this.items;
+  }
+
+  // 获取商品运费价格 使用HttpClient 的get() 方法
+  getShippingPrices(){
+    return this.http.get<{type:string,price:number}[]>('/assets/shipping.json')
   }
 
 }
